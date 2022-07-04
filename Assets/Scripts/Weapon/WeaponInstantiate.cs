8e566504b12015f1a2dfface269b3b9f97a7c465
Jesús Rioja Bravo <57917608+Jesus-Rioja@users.noreply.github.com>
2022-07-04 14:29:32 +0200

    some tweaks

--- Assets/Scripts/Weapon/WeaponInstantiate.cs
+++ Assets/Scripts/Weapon/WeaponInstantiate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponInstantiate : WeaponBase
+public class WeaponInstantiate : WeaponMelee
 {
     [SerializeField] GameObject prefabProyectil;
     [SerializeField] Transform shootPoint;
@@ -18,7 +18,7 @@ public class WeaponInstantiate : WeaponBase
 
     public override void Shot()
     {
-        if (ShotAllowed)
+        if (ShotAllowed && UseAmmo() == UseAmmoResult.ShotMade)
         {
             GameObject proyectil = Instantiate(prefabProyectil, shootPoint.position, shootPoint.rotation);
             proyectil.GetComponent<Rigidbody>()?.AddForce(shootPoint.forward * forceToApplyOnShot);
